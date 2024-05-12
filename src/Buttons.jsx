48d11553a1a60482ddee39c3c2a377e799beaf06
theblaisecode <theblaisecode@gmail.com>
2024-05-12 11:53:56 +0100

    first commit

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,25 +1,23 @@
 export default function Buttons() {
   return (
     <div className="buttons">
-      <button className="email">
-        <a
-          href="mailto:theblaisecode@gmail.com"
-          target="_blank"
-          aria-label="Send an email to TheBlaiseCode">
-          <i className="fa-solid fa-envelope"></i>
-          Email
-        </a>
-      </button>
+      <a
+        className="email btn"
+        href="mailto:theblaisecode@gmail.com"
+        target="_blank"
+        aria-label="Send an email to TheBlaiseCode">
+        <i className="fa-solid fa-envelope"></i>
+        Email
+      </a>
 
-      <button className="linkedin">
-        <a
-          href="https://www.linkedin.com/in/theblaisecode"
-          target="_blank"
-          aria-label="Link to TheBlaiseCode linkedin">
-          <i className="fa-brands fa-linkedin"></i>
-          LinkedIn
-        </a>
-      </button>
+      <a
+        className="linkedin btn"
+        href="https://www.linkedin.com/in/theblaisecode"
+        target="_blank"
+        aria-label="Link to TheBlaiseCode linkedin">
+        <i className="fa-brands fa-linkedin"></i>
+        LinkedIn
+      </a>
     </div>
   );
 }
